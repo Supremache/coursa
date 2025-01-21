204ec6bd7ab3a6558175fc469d5b5e5564331ff4
Abdulrahman Othman <86932829+Supremache@users.noreply.github.com>
2025-01-21 22:57:31 +0200

    Update page.tsx

--- src/app/profile/courses/page.tsx
+++ src/app/profile/courses/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from "@/components/ui/pagination";
